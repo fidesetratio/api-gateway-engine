91299e8cb7866919bbc5020e05e2b82e218ba671
Patar.Tambunan <Patar.Tambunan@itpega01.SINARMASMSIGLIFE.CO.ID>
2019-04-24 14:49:10 +0700

    add new path management

--- src/main/java/com/application/utils/Utility.java
+++ src/main/java/com/application/utils/Utility.java
@@ -8,7 +8,27 @@ public class Utility {
 	
 	public static String calculatePath(String url) {
 		// TODO Auto-generated method stub
+		logger.info("url to calculate:"+url);
+		String segment[] = url.split("/");
+		if(segment.length>0 && !url.startsWith("/gwadmin")){
+			if(segment.length ==4){
+				if(segment[1] != null && segment[2] != null && segment[3] != null){
+					String cat=segment[1];
+					String urlCalculated = segment[2];
+					String urlCatThree = segment[3];
+					logger.info("calculate path:"+urlCalculated);
+					return "/"+cat+"/"+urlCalculated+"/"+urlCatThree+"/**";
+				};
+		};
+		}
+		return url;
+	}
+	
+	
+	public static String calculatePathFront(String url) {
 		
+		// TODO Auto-generated method stub
+		logger.info("url to calculate:"+url);
 		String segment[] = url.split("/");
 		if(segment.length>0 && !url.startsWith("/gwadmin")){
 			String cat=segment[1];
