3637dec9615902e6cdc871a187432c4f45518905
root <root@patartimotius01>
2019-05-01 01:04:41 +0700

    add new procedure in bash

--- stop.sh
+++ stop.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 if [ -f /home/patartimotius/applicationtesting/apigateway/pid.file ]; then
+	if [ps -p /home/patartimotius/applicationtesting/apigateway/pid.file > /dev/null];then
+	
 	kill $(cat /home/patartimotius/applicationtesting/apigateway/pid.file)
 	echo 'kill them...'
+	
+	fi
 fi
