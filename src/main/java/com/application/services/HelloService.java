f730ec1ec0f146c2fb66be3853128d4f10f5d9b6
root <root@patartimotius01>
2019-05-26 22:06:51 +0700

    commit

---
+++ src/main/java/com/application/services/HelloService.java
@@ -0,0 +1,11 @@
+package com.application.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+	public String get() {
+		return "Hello World";
+	}
+}
