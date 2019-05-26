3c29c524783f39fa6a96e343163df819a9a0e338
root <root@patartimotius01>
2019-05-26 22:07:05 +0700

    commit

--- src/main/java/com/application/services/RefreshLocator.java
+++ src/main/java/com/application/services/RefreshLocator.java
@@ -5,15 +5,17 @@ import org.springframework.cloud.netflix.zuul.RoutesRefreshedEvent;
 import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+
+import com.application.locator.DatabaseRouteLocator;
   
 @Service
 public class RefreshLocator {
 	
 	@Autowired
-    ApplicationEventPublisher publisher;
+    private ApplicationEventPublisher publisher;
  
     @Autowired
-    RouteLocator routeLocator;
+    private DatabaseRouteLocator routeLocator;
  
     public void refreshRoute() {
         RoutesRefreshedEvent routesRefreshedEvent = new RoutesRefreshedEvent(routeLocator);
