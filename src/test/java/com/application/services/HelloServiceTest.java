f5e8cfd72a83db00b795a4eb8a3d855cd8be74fd
root <root@patartimotius01>
2019-05-26 22:05:58 +0700

    commit

---
+++ src/test/java/com/application/services/HelloServiceTest.java
@@ -0,0 +1,22 @@
+package com.application.services;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+@AutoConfigureMockMvc
+public class HelloServiceTest {
+
+		@Test
+		public void test() {
+			assertTrue(true);
+		}
+}
