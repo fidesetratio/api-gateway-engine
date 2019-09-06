4a8a2513a7e2e9029ed6ac4fde5e4d3971897410
Patar.Tambunan <Patar.Tambunan@itpega01.SINARMASMSIGLIFE.CO.ID>
2019-09-06 17:16:22 +0700

    aad

---
+++ src/main/java/com/application/security/authentication/PermitAllAuthentication.java
@@ -0,0 +1,55 @@
+package com.application.security.authentication;
+
+import java.util.Collection;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+
+public class PermitAllAuthentication implements Authentication{
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "gaga";
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return AuthorityUtils.createAuthorityList("ALLOWED");
+		
+	}
+
+	@Override
+	public Object getCredentials() {
+		// TODO Auto-generated method stub
+		return new Object();
+	}
+
+	@Override
+	public Object getDetails() {
+		// TODO Auto-generated method stub
+		return "";
+	}
+
+	@Override
+	public Object getPrincipal() {
+		// TODO Auto-generated method stub
+		return "gaa";
+	}
+
+	@Override
+	public boolean isAuthenticated() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public void setAuthenticated(boolean isAuthenticated)
+			throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
