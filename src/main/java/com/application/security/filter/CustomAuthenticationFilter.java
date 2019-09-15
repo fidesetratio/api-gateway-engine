1e25a701ded81edb0486c42c9a50a673375139ee
root <root@patartimotius01>
2019-09-15 19:02:39 +0700

    p

--- src/main/java/com/application/security/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/application/security/filter/CustomAuthenticationFilter.java
@@ -10,7 +10,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.assertj.core.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
